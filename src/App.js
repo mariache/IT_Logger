c554f3584794d02b44c7f5806c1ea3cf2352ce15
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-28 00:41:31 +1200

    - materialize css and js setup;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    //initialize materialize js
+    M.AutoInit();
+  });
   return <div className="App">IT logger</div>;
-}
+};
 
 export default App;
