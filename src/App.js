8bf4dcc9886163fa5eea8b32ebac8214562a88f8
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-28 23:46:54 +1200

    - Added AddTechModal;

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
 
 const App = () => {
   useEffect(() => {
@@ -21,6 +22,7 @@ const App = () => {
         <AddBtn />
         <AddLogModal />
         <EditLogModal />
+        <AddTechModal />
       </div>
     </>
   );
