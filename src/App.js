b89f719f50ea1ee694f47006e0d4809353cedcde
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-30 23:39:58 +1200

    - added redux, thunk, reducers, - added store;

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   useEffect(() => {
@@ -16,17 +18,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <Logs />
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-      </div>
-    </>
+    <Provider store={store}>
+      <>
+        <NavBar />
+        <div className="container">
+          <Logs />
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+        </div>
+      </>
+    </Provider>
   );
 };
 
